e61ede18cfb6474b6f725df9f31afb8ed167fdb3
mediba-kitada <kitada@mediba.jp>
2014-05-23 18:33:27 +0900

    add aws301 node

--- chef-repo/site-cookbooks/docker/recipes/default.rb
+++ chef-repo/site-cookbooks/docker/recipes/default.rb
@@ -11,3 +11,11 @@ package "docker-io" do
   options "--enablerepo=epel"
   action :install
 end
+
+bash "dockerup-coupy" do
+  only_if "find /vagrant/docker/coupy/Dockerfile"
+  code <<-EOL
+    docker build -t lucky/coupy /vagrant/docker/coupy
+    docker run -p 49001:22 -d lucky/coupy
+  EOL
+end
