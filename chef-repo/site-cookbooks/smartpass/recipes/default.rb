0b56d0186713cd4372956d997bdc2e103e8e0d15
mediba-kitada <kitada@mediba.jp>
2014-06-27 17:01:51 +0900

    smartpassリポジトリ(旧資産)のDB設定レシピを追加

--- chef-repo/site-cookbooks/smartpass/recipes/default.rb
+++ chef-repo/site-cookbooks/smartpass/recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe 'database::mysql'
+
 link "/var/www/pass.auone.jp" do
   to "/vagrant/smartpass/pass.auone.jp"
 end
@@ -23,4 +25,38 @@ cookbook_file "/var/log/ap/coupon-detail.log" do
   owner "apache"
   group "apache"
   mode 00755
+end
+
+# 接続情報
+mysql_connection_info = {
+  :host => "localhost",
+  :username => "root",
+}
+
+# BDB.pass
+mysql_database "pass" do
+  connection mysql_connection_info
+  action :create
+end
+
+mysql_database_user "cake_view" do
+  connection mysql_connection_info
+  password "cake_view"
+  database_name "pass"
+  privileges [:all]
+  action [:create, :grant]
+end
+
+# async.pass
+mysql_database "pass_async" do
+  connection mysql_connection_info
+  action :create
+end
+
+mysql_database_user "sirius_select" do
+  connection mysql_connection_info
+  password "select"
+  database_name "pass_async"
+  privileges [:all]
+  action [:create, :grant]
 end
